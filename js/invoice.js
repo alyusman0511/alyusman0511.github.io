9e300db59e1e6373ac9143b0acde0970a00d7929
alyusman0511 <108605792+alyusman0511@users.noreply.github.com>
2022-07-04 08:57:19 +0700

    4

--- js/invoice.js
+++ js/invoice.js
@@ -132,7 +132,7 @@ new Vue({
          margin: { left: 1.5, top: 7.0 }
        });
        let ln = (this.keranjangBarang.length * 0.8)+8.3;
-       console.log('ini ln: ' ,ln);
+       console.log('ini ln: ' ,this.keranjangBarang.length);
        doc.text('Total : '+this.formatRupiah(this.totalBelanja, 'Rp.'), 15, ln);
        doc.text('Terbilang: sepuluh juta limaratus delapan puluh ribu rupiah', 1.5, ln, { align: "left", maxWidth: "15.5" });
       // Using array of sentences
