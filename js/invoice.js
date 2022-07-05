77b0ae891b5b14ceba0f1e512db4afe89652466b
alyusman0511 <108605792+alyusman0511@users.noreply.github.com>
2022-07-05 12:18:22 +0700

    Update invoice.js

--- js/invoice.js
+++ js/invoice.js
@@ -5,8 +5,10 @@ new Vue({
     kop: {
       nama: null,
       alamat: null,
-      invoiceKe: 0,
-      tgl: null,
+      telepon: null,
+      invoiceKe: null,
+      tgl: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      jthTempo: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)
     },
     keranjangBarang: [],
     add: {
@@ -19,35 +21,51 @@ new Vue({
     },
     totalBelanjaAngka: 0,
     totalBelanjaString: null,
-    totalBelanjaTerbilang: null,
+    totalBelanjaTerbilang: 'Nol',
     dialogEdit: false,
 
     ////
-    htmlhidden: false
+    date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    menu: false,
+    date1: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    menu1: false,
+  },
+  computed: {
+    computedDateFormattedMomentjs() {
+      moment.locale('id');
+      return this.date ? moment(this.date).format('Do MMMM YYYY') : ''
+    },
+    computedDateFormattedMomentjs1() {
+      moment.locale('id');
+      return this.date1 ? moment(this.date1).format('Do MMMM YYYY') : ''
+    },
   },
   mounted() {
     this.$refs.refKopNama.focus();
     this.totalBelanjaString = this.formatRupiah(this.totalBelanjaAngka.toString(), 'Rp.')
-    this.totalBelanjaTerbilang = this.terbilang(parseInt(this.totalBelanjaAngka));
+    //this.totalBelanjaTerbilang = this.terbilang(parseInt(this.totalBelanjaAngka));
   },
 
   methods: {
-    focusInputAlamat(){
+    focusInputAlamat() {
       this.$refs.refKopAlamat.focus();
     },
-    focusInputNoInvoice(){
+    focusInputNoTelepon() {
+      this.$refs.refKopTelepon.focus();
+    },
+    focusInputNoInvoice() {
       this.$refs.refKopInvoiceKe.focus();
     },
-    focusInputNamaBarang(){
+    focusInputNamaBarang() {
       this.$refs.refNamaBarang.focus();
     },
-    focusInputQty(){
+    focusInputQty() {
       this.$refs.inputQty.focus();
     },
-    focusInputSatuan(){
+    focusInputSatuan() {
       this.$refs.inputSatuan.focus();
     },
-    focusInputHarga(){
+    focusInputHarga() {
       this.$refs.inputHarga.focus();
     },
     addKeranjang() {
@@ -86,10 +104,26 @@ new Vue({
         this.totalBelanjaAngka += parseInt(element.amount)
       }
       this.totalBelanjaString = this.formatRupiah(this.totalBelanjaAngka.toString(), 'Rp.');
-      this.totalBelanjaTerbilang = this.terbilang(this.totalBelanjaAngka); //angkaTerbilang(this.totalBelanjaAngka);//
+      const bb = angkaTerbilang(this.totalBelanjaAngka);
+      let b1 = bb.split(' ');
+      b2 = this.kapitalisasiKata(b1[0]);
+      let b3 = [b2];
+      let c1 = b1.splice(1);
+      for(let i = 0; i < c1.length; i++){
+        b3.push(c1[i])
+      }
+      this.totalBelanjaTerbilang = b3.join(' ');//this.kapitalisasiKata(angkaTerbilang(this.totalBelanjaAngka));//this.terbilang(this.totalBelanjaAngka); //
+      console.log('hasil b3', b3.join(' '));
       console.log('total angka : ', this.totalBelanjaAngka);
-      console.log('total string: ' , this.totalBelanjaString);
-      console.log('total terbilang: ', this.totalBelanjaTerbilang);
+      console.log('total string: ', this.totalBelanjaString);
+      console.log('total terbilang: ', this.totalBelanjaTerbilang);//this.totalBelanjaTerbilang);
+    },
+    kapitalisasiKata(str) {
+      return str.replace(/\w\S*/g, function (kata) {
+        const kataBaru = kata.slice(0, 1).toUpperCase() + kata.substr(1);
+        return kataBaru
+      });
+
     },
     setEdit(item) {
       this.add.no = item.no
@@ -129,16 +163,15 @@ new Vue({
       }
       this.totalFinal()
     },
-    simpanPdf() {
-      
+    lihatPdf() {
+      console.log('inig kop.tgl :', this.kop.tgl);
+      console.log('inig kop.jthTempo :', this.kop.jthTempo);
+
       if (this.kop.nama == '' || this.kop.invoiceKe == '0') return false;
       const d = new Date();
-      const th = d.getFullYear();
-      const bl = d.getMonth().toString();
-      const bbl = (bl.length < 2) ? '0' + bl : bl;
-      const tg = d.getDay().toString();
-      const ttg = (tg.length < 2) ? '0' + tg : tg;
-      this.kop.tgl = th + '/' + bbl + '/' + ttg;
+      moment.locale('id');
+      this.kop.tgl = moment(this.date).format('Do MMMM YYYY');
+      this.kop.jthTempo = moment(this.date1).format('Do MMMM YYYY');
 
       const columns = [
         { title: "No.", dataKey: "no" },
@@ -146,7 +179,7 @@ new Vue({
         { title: "Satuan", dataKey: "satuan" },
         { title: "Keterangan", dataKey: "description" },
         { title: "Harga", dataKey: "unitPriceTampil" },
-        { title: "Subtotal", dataKey: "amountTampil" }
+        { title: "Subtotal", dataKey: "amountTampil" },
       ];
       const doc = new jsPDF({
         orientation: "portrait",
@@ -155,22 +188,22 @@ new Vue({
       });
       // text is placed using x, y coordinates
       doc.setFontSize(14).setFontStyle("bold").text('TOKO PLASTIK SAYA', 1.5, 1.5);
-      doc.setFontSize(12).setFontStyle("normal").text('PASURUAN \n(+62)81336554778', 1.5, 2.5);
-      doc.setFontSize(14).setFontStyle("bold").text('FAKTUR', 12.0, 1.5);
-      doc.setFontSize(12).setFontStyle("bold").text(`Yth. Pelanggan`, 1.5, 4.5);
-      doc.setFontSize(12).setFontStyle("normal").text(`\n${this.kop.nama.toString().toUpperCase()}\n${this.kop.alamat}`, 1.5, 4.5);
+      doc.setFontSize(12).setFontStyle("normal").text('PASURUAN \nTELP. 081336554778', 1.5, 2.5);
+      doc.setFontSize(14).setFontStyle("bold").text('FAKTUR', 17.0, 1.5);
+      doc.setFontSize(12).setFontStyle("bold").text(`KEPADA YTH.`, 1.5, 4.5);
+      doc.setFontSize(12).setFontStyle("normal").text(`\n${this.kop.nama.toString().toUpperCase()}\n${this.kapitalisasiKata(this.kop.alamat)}\n${this.kop.telepon}`, 1.5, 4.5);
 
-      doc.setFontSize(12).setFontStyle("bold").text(`No. Faktur \nTanggal Faktur`, 12, 4.5);
-      doc.setFontSize(12).setFontStyle("normal").text(`${this.kop.invoiceKe}` + `\n${this.kop.tgl}`, 17, 4.5);
+      doc.setFontSize(12).setFontStyle("bold").text(`NO. FAKTUR \nTANGGAL/KIRIM \nTGL. JATUH TEMPO`, 12, 4.5);
+      doc.setFontSize(12).setFontStyle("normal").text(`${this.kop.invoiceKe}` + `\n${this.kop.tgl}` + `\n${this.kop.jthTempo}`, 17, 4.5);
       // create a line under heading
       //titik awal garis , posisi tinggi garis , panjang , titik akhir garis
       // doc.setLineWidth(0.01).line(0.5, 0.6, 8.0, 0.6);
       // Using autoTable plugin
       let bodycolom = [];
-      for(let i = 0; i < this.keranjangBarang.length; i++){
+      for (let i = 0; i < this.keranjangBarang.length; i++) {
         const el = this.keranjangBarang[i];
         const dorong = {
-          no: i+1,
+          no: i + 1,
           qtyTampil: el.qtyTampil,
           satuan: el.satuan,
           description: el.description,
@@ -182,37 +215,117 @@ new Vue({
       doc.autoTable({
         columns,
         body: bodycolom, //this.keranjangBarang,
-        margin: { left: 1.5, top: 7.0 }
+        margin: { left: 1.5, top: 7.0 },
+        columnStyles: {
+          'unitPriceTampil': { halign: "right" },
+          'amountTampil': { halign: "right" },
+        },
+
       });
-      let ln = (this.keranjangBarang.length * 0.8) + 8.3;
+      let ln = (this.keranjangBarang.length * 0.8) + 8.5;
       console.log('ini ln: ', this.keranjangBarang.length);
-      doc.text('  Total: ' + this.totalBelanjaString+',00', 14, ln);
-      doc.setFontStyle("normal").text('Terbilang: '+this.terbilang(this.totalBelanjaAngka)+' Rupiah.', 1.5, ln, { align: "left", maxWidth: "11.5" });
-      // Using array of sentences
-      /* doc
-         .setFont("helvetica")
-         .setFontSize(12)
-         .text(this.moreText, 0.5, 3.5, { align: "left", maxWidth: "7.5" });
-       
-      // Creating footer and saving file
+      doc.text('  Total: ' + this.totalBelanjaString + ',00', 15, ln);
+      doc.setFontStyle("normal").text('Terbilang: ' + this.terbilang(this.totalBelanjaAngka) + ' Rupiah.', 1.5, ln, { align: "left", maxWidth: "11.5" });
+
       doc
-        .setFont("times")
-        .setFontSize(11)
-        .setFontStyle("italic")
-        .setTextColor(0, 0, 255)
-        .text(
-          "This is a simple footer located .5 inches from page bottom",
-          0.5,
-          doc.internal.pageSize.height - 0.5
-        ); */
+        .setFontSize(12)
+        .setFontStyle('bold')
+        .text('PEMBAYARAN DITRANSFER KE                                        TANDA TERIMA', 2.5, ln + 2);
+      doc.setLineWidth(0.02).line(2.5, ln + 2.1, 9.2, ln + 2.1);
+      doc
+        .setFontSize(12)
+        .setFontStyle('normal')
+        .text('BCA. 0890800521 \nBNI. 0346396475 \na.n BAMBANG', 2.5, ln + 2.7);
+
       /*
-doc.output('save', 'filename.pdf'); //Try to save PDF as a file (not works on ie before 10, and some mobile devices)
-doc.output('datauristring');        //returns the data uri string
-doc.output('datauri');              //opens the data uri in current window
-doc.output('dataurlnewwindow');     //opens the data uri in new window
-*/
-      //.save(`${this.heading}.pdf`);
+      doc.output('save', 'filename.pdf'); //Try to save PDF as a file (not works on ie before 10, and some mobile devices)
+      doc.output('datauristring');        //returns the data uri string
+      doc.output('datauri');              //opens the data uri in current window
+      doc.output('dataurlnewwindow');     //opens the data uri in new window
+      */
       doc.output('dataurlnewwindow', `judul.pdf`);
+    },
+    simpanPdf() {
+      console.log('inig kop.tgl :', this.kop.tgl);
+      console.log('inig kop.jthTempo :', this.kop.jthTempo);
+
+      if (this.kop.nama == '' || this.kop.invoiceKe == '0') return false;
+      const d = new Date();
+      moment.locale('id');
+      this.kop.tgl = moment(this.date).format('Do MMMM YYYY');
+      this.kop.jthTempo = moment(this.date1).format('Do MMMM YYYY');
+
+      const columns = [
+        { title: "No.", dataKey: "no" },
+        { title: "QTY", dataKey: "qtyTampil" },
+        { title: "Satuan", dataKey: "satuan" },
+        { title: "Keterangan", dataKey: "description" },
+        { title: "Harga", dataKey: "unitPriceTampil" },
+        { title: "Subtotal", dataKey: "amountTampil" },
+      ];
+      const doc = new jsPDF({
+        orientation: "portrait",
+        unit: "cm",
+        format: "a4"//letter"
+      });
+      // text is placed using x, y coordinates
+      doc.setFontSize(14).setFontStyle("bold").text('TOKO PLASTIK SAYA', 1.5, 1.5);
+      doc.setFontSize(12).setFontStyle("normal").text('PASURUAN \nTELP. 081336554778', 1.5, 2.5);
+      doc.setFontSize(14).setFontStyle("bold").text('FAKTUR', 17.0, 1.5);
+      doc.setFontSize(12).setFontStyle("bold").text(`KEPADA YTH.`, 1.5, 4.5);
+      doc.setFontSize(12).setFontStyle("normal").text(`\n${this.kop.nama.toString().toUpperCase()}\n${this.kapitalisasiKata(this.kop.alamat)}\n${this.kop.telepon}`, 1.5, 4.5);
+
+      doc.setFontSize(12).setFontStyle("bold").text(`NO. FAKTUR \nTANGGAL/KIRIM \nTGL. JATUH TEMPO`, 12, 4.5);
+      doc.setFontSize(12).setFontStyle("normal").text(`${this.kop.invoiceKe}` + `\n${this.kop.tgl}` + `\n${this.kop.jthTempo}`, 17, 4.5);
+      // create a line under heading
+      //titik awal garis , posisi tinggi garis , panjang , titik akhir garis
+      // doc.setLineWidth(0.01).line(0.5, 0.6, 8.0, 0.6);
+      // Using autoTable plugin
+      let bodycolom = [];
+      for (let i = 0; i < this.keranjangBarang.length; i++) {
+        const el = this.keranjangBarang[i];
+        const dorong = {
+          no: i + 1,
+          qtyTampil: el.qtyTampil,
+          satuan: el.satuan,
+          description: el.description,
+          unitPriceTampil: el.unitPriceTampil,
+          amountTampil: el.amountTampil
+        }
+        bodycolom.push(dorong)
+      }
+      doc.autoTable({
+        columns,
+        body: bodycolom, //this.keranjangBarang,
+        margin: { left: 1.5, top: 7.0 },
+        columnStyles: {
+          'unitPriceTampil': { halign: "right" },
+          'amountTampil': { halign: "right" },
+        },
+
+      });
+      let ln = (this.keranjangBarang.length * 0.8) + 8.5;
+      console.log('ini ln: ', this.keranjangBarang.length);
+      doc.text('  Total: ' + this.totalBelanjaString + ',00', 15, ln);
+      doc.setFontStyle("normal").text('Terbilang: ' + this.terbilang(this.totalBelanjaAngka) + ' Rupiah.', 1.5, ln, { align: "left", maxWidth: "11.5" });
+
+      doc
+        .setFontSize(12)
+        .setFontStyle('bold')
+        .text('PEMBAYARAN DITRANSFER KE                                        TANDA TERIMA', 2.5, ln + 2);
+      doc.setLineWidth(0.02).line(2.5, ln + 2.1, 9.2, ln + 2.1);
+      doc
+        .setFontSize(12)
+        .setFontStyle('normal')
+        .text('BCA. 0890800521 \nBNI. 0346396475 \na.n BAMBANG', 2.5, ln + 2.7);
+
+      /*
+      doc.output('save', 'filename.pdf'); //Try to save PDF as a file (not works on ie before 10, and some mobile devices)
+      doc.output('datauristring');        //returns the data uri string
+      doc.output('datauri');              //opens the data uri in current window
+      doc.output('dataurlnewwindow');     //opens the data uri in new window
+      */
+      doc.output('save', `${this.kop.invoiceKe}.pdf`);
     },
     /* Fungsi formatRupiah */
     formatRupiah(angka, prefix) {
@@ -247,50 +360,50 @@ doc.output('dataurlnewwindow');     //opens the data uri in new window
       rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
       return rupiah;
     },
-    terbilang(nilai){
-        nilai = Math.abs(nilai);
-        let simpanNilaiBagi=0;
-        const huruf = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
-        let temp="";
-     
-        if (nilai < 12) {
-            temp = " "+huruf[nilai];
-        }
-        else if (nilai <20) {
-            temp = this.terbilang(nilai - 10) + " Belas";
-        }
-        else if (nilai < 100) {
-            simpanNilaiBagi = Math.floor(nilai/10);
-            temp = this.terbilang(simpanNilaiBagi)+" Puluh"+ this.terbilang(nilai % 10);
-        }
-        else if (nilai < 200) {
-            temp = " Seratus" + terbilang(nilai - 100);
-        }
-        else if (nilai < 1000) {
-            simpanNilaiBagi = Math.floor(nilai/100);
-            temp = this.terbilang(simpanNilaiBagi) + " Ratus" + this.terbilang(nilai % 100);
-        }
-         else if (nilai < 2000) {
-            temp = " Seribu" + this.terbilang(nilai - 1000);
-        }
-        else if (nilai < 1000000) {
-            simpanNilaiBagi = Math.floor(nilai/1000);
-            temp = this.terbilang(simpanNilaiBagi) + " Ribu" + this.terbilang(nilai % 1000);
-        } 
-        else if (nilai < 1000000000) {
-            simpanNilaiBagi = Math.floor(nilai/1000000);
-            temp = this.terbilang(simpanNilaiBagi) + " Juta" + this.terbilang(nilai % 1000000);
-        } 
-        else if (nilai < 1000000000000) {
-            simpanNilaiBagi = Math.floor(nilai/1000000000);
-            temp = this.terbilang(simpanNilaiBagi) + " Miliar" + this.terbilang(nilai % 1000000000);
-        } 
-        else if (nilai < 1000000000000000) {
-            simpanNilaiBagi = Math.floor(nilai/1000000000000);
-            temp = this.terbilang(nilai/1000000000000) + " Triliun" + this.terbilang(nilai % 1000000000000);
-        }
-     
-        return temp;
+    terbilang(nilai) {
+      nilai = Math.abs(nilai);
+      let simpanNilaiBagi = 0;
+      const huruf = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+      let temp = "";
+
+      if (nilai < 12) {
+        temp = " " + huruf[nilai];
+      }
+      else if (nilai < 20) {
+        temp = this.terbilang(nilai - 10) + " Belas";
+      }
+      else if (nilai < 100) {
+        simpanNilaiBagi = Math.floor(nilai / 10);
+        temp = this.terbilang(simpanNilaiBagi) + " Puluh" + this.terbilang(nilai % 10);
+      }
+      else if (nilai < 200) {
+        temp = " Seratus" + this.terbilang(nilai - 100);
+      }
+      else if (nilai < 1000) {
+        simpanNilaiBagi = Math.floor(nilai / 100);
+        temp = this.terbilang(simpanNilaiBagi) + " Ratus" + this.terbilang(nilai % 100);
+      }
+      else if (nilai < 2000) {
+        temp = " Seribu" + this.terbilang(nilai - 1000);
+      }
+      else if (nilai < 1000000) {
+        simpanNilaiBagi = Math.floor(nilai / 1000);
+        temp = this.terbilang(simpanNilaiBagi) + " Ribu" + this.terbilang(nilai % 1000);
+      }
+      else if (nilai < 1000000000) {
+        simpanNilaiBagi = Math.floor(nilai / 1000000);
+        temp = this.terbilang(simpanNilaiBagi) + " Juta" + this.terbilang(nilai % 1000000);
+      }
+      else if (nilai < 1000000000000) {
+        simpanNilaiBagi = Math.floor(nilai / 1000000000);
+        temp = this.terbilang(simpanNilaiBagi) + " Miliar" + this.terbilang(nilai % 1000000000);
+      }
+      else if (nilai < 1000000000000000) {
+        simpanNilaiBagi = Math.floor(nilai / 1000000000000);
+        temp = this.terbilang(nilai / 1000000000000) + " Triliun" + this.terbilang(nilai % 1000000000000);
+      }
+
+      return temp;
     },
 
   }
